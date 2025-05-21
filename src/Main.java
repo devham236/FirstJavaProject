93b6f804286cf51ab169d51154eb4291eafdd907
Hamza Mirza <hamza665@hotmail.de>
2025-05-21 13:30:52 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
 
     public static void main(String[] args){
-        // Comment
-
-        System.out.println("Hello World");
-        System.out.println("My name is Hamza");
+        System.out.println("Hello");
     }
 
 }
