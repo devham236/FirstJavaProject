ffdcd11e706d585eb9f605b644e6e6042ad0911c
Hamza Mirza <hamza665@hotmail.de>
2025-05-21 10:07:07 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args){
+        // Comment
+
+        System.out.println("Hello World");
+        System.out.println("Hello World again");
+        System.out.println("My name is Hamza");
+    }
+
+}
