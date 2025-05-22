c9562977d3fbe1fcdb8b8703da53f7a2552bbf02
Hamza Mirza <hamza665@hotmail.de>
2025-05-22 09:16:05 +0200

    primitve data types

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
 
     public static void main(String[] args){
+
+        int age = 27;
+        int year = 2025;
+        int quantity = 1;
+
+        double price = 9.99;
+        double score = 8.9;
+        double temperature = 32.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '€';
+
+        boolean isStudent = false;
+        boolean forSale = true;
+        boolean isOnline = false;
+
     }
 
 }
