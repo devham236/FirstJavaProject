3e3c9bdbf39d5cacf08394bbe559b467bd29cda5
Hamza Mirza <hamza665@hotmail.de>
2025-05-22 11:27:08 +0200

    how to work with user input

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
-        int age = 27;
-        int year = 2025;
-        int quantity = 1;
+        double width = 0;
+        double height = 0;
+        double area = 0;
 
-        double price = 9.99;
-        double score = 8.9;
-        double temperature = 32.5;
+        System.out.print("What is the width of the rectangle:");
+        width = scanner.nextDouble();
 
-        char grade = 'A';
-        char symbol = '!';
-        char currency = '€';
+        System.out.print("What is the height of the rectangle:");
+        height = scanner.nextDouble();
 
-        boolean isStudent = false;
-        boolean forSale = true;
-        boolean isOnline = false;
+        area = width * height;
 
+        System.out.print("Your area is: " + area);
+
+        scanner.close();
     }
 
 }
