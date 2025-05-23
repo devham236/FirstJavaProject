0de922cf743e48e520592a43360455655057f43c
Hamza Mirza <hamza665@hotmail.de>
2025-05-23 09:59:51 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        //Madlibs game
+        //Madlibs Game
         Scanner scanner = new Scanner(System.in);
 
         String adjective1;
