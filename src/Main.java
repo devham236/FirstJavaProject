e9902c3ea795154ed08e14d91ab4b328465b1e08
Hamza Mirza <hamza665@hotmail.de>
2025-05-23 10:29:46 +0200

    added more information to order of operations

--- src/Main.java
+++ src/Main.java
@@ -15,14 +15,14 @@ public class Main {
 
         // Order of Operations --> "P-E-M-D-A-S"
 
-        // 1. P = Parenthesis
-        // 2. E = Exponent
-        // 3. M = Multiplication
-        // 4. D = Division
-        // 5. A = Addition
-        // 6. S = Substraction
-
         double result = 3 + 4 * (7 - 5) / 2.0;
+
+        // 1. P = Parenthesis       3 + 4 * 2 / 2.0
+        // 2. E = Exponent          3 + 4 * 2 / 2.0 --> no exponent
+        // 3. M = Multiplication    3 + 8 / 2.0
+        // 4. D = Division          3 + 4
+        // 5. A = Addition          7
+        // 6. S = Substraction      7 --> no substraction
 
         System.out.println(result);
     }
