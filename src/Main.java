212041e12bc9df86cc4226a6c40b223caaa38530
Hamza Mirza <hamza665@hotmail.de>
2025-05-23 09:56:54 +0200

    madlibs game

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,31 @@ public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        double width = 0;
-        double height = 0;
-        double area = 0;
+        String adjective1;
+        String adjective2;
+        String adjective3;
+        String noun1;
+        String verb1;
 
-        System.out.print("What is the width of the rectangle:");
-        width = scanner.nextDouble();
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
 
-        System.out.print("What is the height of the rectangle:");
-        height = scanner.nextDouble();
+        System.out.print("Enter a noun (animal or person): ");
+        noun1 = scanner.nextLine();
 
-        area = width * height;
+        System.out.print("Enter an adjective (description): ");
+        adjective2 = scanner.nextLine();
 
-        System.out.print("Your area is: " + area);
+        System.out.print("Enter a verb ending with -ing (action): ");
+        verb1 = scanner.nextLine();
+
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
+
+        System.out.print("\nToday i went to a " + adjective1 + " zoo.");
+        System.out.print(" In an exhibit, I saw a " + noun1 + ".");
+        System.out.print(" " + noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.print(" I was " + adjective3 + "!");
 
         scanner.close();
     }
