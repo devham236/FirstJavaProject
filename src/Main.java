59600177fb69d8fc12b96ced0eafa3a0292c0228
Hamza Mirza <hamza665@hotmail.de>
2025-05-23 11:15:04 +0200

    shopping cart program

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-        // Arithmetic Operators
-        int x = 7;
-        int y = 2;
+        // Shopping Cart Program
+        Scanner scanner = new Scanner(System.in);
 
-        // result = x + y;
-        // result = x - y;
-        // result = x * y;
-        // result = x / y;
+        String item;
+        double price;
+        int amount;
+        double total;
 
-        // result = x % y;
+        System.out.print("What item would you like to buy?: ");
+        item = scanner.nextLine();
 
+        System.out.print("What is the price for each?: ");
+        price = scanner.nextDouble();
 
-        // Order of Operations --> "P-E-M-D-A-S"
+        System.out.print("How many would you like?: ");
+        amount = scanner.nextInt();
 
-        double result = 3 + 4 * (7 - 5) / 2.0;
+        total = price * amount;
 
-        // 1. P = Parenthesis       3 + 4 * 2 / 2.0
-        // 2. E = Exponent          3 + 4 * 2 / 2.0 --> no exponent
-        // 3. M = Multiplication    3 + 8 / 2.0
-        // 4. D = Division          3 + 4
-        // 5. A = Addition          7
-        // 6. S = Substraction      7 --> no substraction
+        System.out.print("\nYou have bought " + amount + " " + item + "/s");
+        System.out.print("\nYour total is $" + total);
 
-        System.out.println(result);
+        scanner.close();
     }
 
 }
