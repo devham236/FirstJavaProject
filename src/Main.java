838f56daf6458c4702aa5cb073d12c082127aec3
Hamza Mirza <hamza665@hotmail.de>
2025-05-23 10:25:09 +0200

    order of operations

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,6 @@ public class Main {
         int x = 7;
         int y = 2;
 
-        int result;
-
         // result = x + y;
         // result = x - y;
         // result = x * y;
@@ -14,7 +12,19 @@ public class Main {
 
         // result = x % y;
 
-        System.out.println();
+
+        // Order of Operations --> "P-E-M-D-A-S"
+
+        // 1. P = Parenthesis
+        // 2. E = Exponent
+        // 3. M = Multiplication
+        // 4. D = Division
+        // 5. A = Addition
+        // 6. S = Substraction
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println(result);
     }
 
 }
