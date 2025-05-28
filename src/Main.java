fe11668edfd89cc3621bc6b56a91ed99740be174
Hamza Mirza <hamza665@hotmail.de>
2025-05-28 09:44:25 +0200

    random double with specified format

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
-import  java.util.Random;
+import java.util.Random;
+import java.text.DecimalFormat;
 
 public class Main {
 
     public static void main(String[] args) {
         // Random numbers
         Random random = new Random();
+        DecimalFormat df = new DecimalFormat("#.##");
 
         double number;
 
         number = random.nextDouble();
 
-        // without specified origin and bound, it generates
+        // without specified origin and bound, 'random.nextDouble()' generates
         // a random double between 0 and 1
+        // with the 'DecimalFormat' class you can decide
+        // how many decimals the double should have
 
-
-        System.out.print(number);
+        System.out.print(df.format(number));
     }
 }
