8521f0e645ac20c13dea909a7ef6b41b54cdff41
Hamza Mirza <hamza665@hotmail.de>
2025-05-28 09:57:18 +0200

    more Math methods

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,10 @@ public class Main {
         result = Math.abs(-5);
         result = Math.sqrt(9);
         result = Math.round(3.14);
+        result = Math.ceil(3.14);
+        result = Math.floor(3.14);
+        result = Math.max(10, 20);
+        result = Math.min(10, 20);
 
 
         System.out.println(result);
