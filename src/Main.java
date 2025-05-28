99d3c2a2d32fc3b5bf684279499be49fd5a27e9e
Hamza Mirza <hamza665@hotmail.de>
2025-05-28 09:34:28 +0200

    random number

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-import java.util.Scanner;
+import  java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+        // Random numbers
+        Random random = new Random();
 
+        int number;
+
+        number = random.nextInt(1, 6);
+        // origin is inclusive --> number 1 can be the ranodm number
+        // bound is exclusive --> number 6 will be excluded
+
+        System.out.print(number);
     }
 }
