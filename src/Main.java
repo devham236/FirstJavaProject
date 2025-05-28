cf99dc13e9c3ee9738cb696380e77ff969f59959
Hamza Mirza <hamza665@hotmail.de>
2025-05-28 09:46:44 +0200

    random booleans

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,17 @@
 import java.util.Random;
-import java.text.DecimalFormat;
 
 public class Main {
 
     public static void main(String[] args) {
         // Random numbers
         Random random = new Random();
-        DecimalFormat df = new DecimalFormat("#.##");
 
-        double number;
+        boolean isHeads;
 
-        number = random.nextDouble();
+        isHeads = random.nextBoolean();
 
-        // without specified origin and bound, 'random.nextDouble()' generates
-        // a random double between 0 and 1
-        // with the 'DecimalFormat' class you can decide
-        // how many decimals the double should have
+        // value is true or false
 
-        System.out.print(df.format(number));
+        System.out.print(isHeads);
     }
 }
