029db922a57c33b7c26988f8485d4f6bbbd18c9a
Hamza Mirza <hamza665@hotmail.de>
2025-05-28 07:59:50 +0200

    clean up

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // if statements
-        int age = -1;
 
-        if (age >= 18){
-            System.out.println("You are an adult.");
-        } else if (age < 0) {
-            System.out.println("You havent been born yet.");
-        } else {
-            System.out.println("You are a child");
-        }
     }
 }
