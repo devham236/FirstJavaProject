b22a72d4d26a16ef2835061046913374ae6d89ca
Hamza Mirza <hamza665@hotmail.de>
2025-05-30 09:15:09 +0200

    printf statement with different data types

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,17 @@ public class Main {
         double height = 60.5;
         boolean isEmployed = true;
 
-        // placeholder % followed by a specified character for the data type, s = String
-        System.out.printf("Hello %s", name);
+        // placeholder % followed by a specified character for the data type,
+        // s = String
+        // c = character
+        // d = integer
+        // f = double
+        // b = boolean
+
+        System.out.printf("Hello %s\n", name);
+        System.out.printf("Your name starts with a %c\n", firstLetter);
+        System.out.printf("You are %d years old\n", age);
+        System.out.printf("You are %f inches tall\n", height);
+        System.out.printf("Employed: %b\n", height);
     }
 }
