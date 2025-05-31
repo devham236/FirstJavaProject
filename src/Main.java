55e77440573c6c3848bfbba7b9446ad8217a23a9
Hamza Mirza <hamza665@hotmail.de>
2025-05-31 10:54:47 +0200

    string methods

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Nested if statement
+        // String methods
+        String name = "Hamza Mirza";
 
-        boolean isStudent = true;
-        boolean isSenior = false;
-        double price = 9.99;
+        int length = name.length();
+        char letter = name.charAt(1);
+        int index = name.indexOf("a");
+        int lastIndex = name.lastIndexOf("a");
 
-        if (isStudent) {
-            if (isSenior){
-                System.out.println("You get a senior discount of 20%");
-                System.out.println("You get a student discount of 10%");
-                price *= 0.7;
-            }
-            else {
-                System.out.println("You get a student discount of 10%");
-                price *= 0.9;
-            }
+        name = name.toUpperCase();
+        name = name.toLowerCase();
+        name = name.trim();
+        name = name.replace("a", "e");
 
-        }
-        else {
-            if (isSenior) {
-                System.out.println("You get a senior discount of 20%");
-                price *= 0.8;
-            }
-            else {
-                price *= 1;
-            }
-        }
-
-        System.out.printf("The price of a ticket is: %.2f€", price);
-
+        System.out.println(name.isEmpty());
     }
 }
