dd4c6d1bf5933f676bb2e34c1a120d8c2f759283
Hamza Mirza <hamza665@hotmail.de>
2025-06-11 10:57:00 +0200

    adjusted logic of calculator program

--- src/Main.java
+++ src/Main.java
@@ -8,32 +8,42 @@ public class Main {
 
         double firstNum;
         double secondNum;
-        double result;
-        String operator;
-        String resultMsg = "Your result:";
+        double result = 0;
+        char operator;
+        boolean validOperation = true;
 
         System.out.print("Enter the first number: ");
         firstNum = scanner.nextDouble();
 
-        System.out.print("Enter an operator: ");
-        operator = scanner.next();
+        System.out.print("Enter an operator (+, -, *, /, ^): ");
+        operator = scanner.next().charAt(0);
 
         System.out.print("Enter the second number: ");
         secondNum = scanner.nextDouble();
 
         switch(operator) {
-            case "+" -> result = firstNum + secondNum;
-            case "-" -> result = firstNum - secondNum;
-            case "*" -> result = firstNum * secondNum;
-            case "/" -> result = firstNum / secondNum;
-            default -> result = 0;
+            case '+' -> result = firstNum + secondNum;
+            case '-' -> result = firstNum - secondNum;
+            case '*' -> result = firstNum * secondNum;
+            case '/' -> {
+                if(secondNum == 0) {
+                    System.out.print("Cannot divide by zero! ");
+                    validOperation = false;
+                }
+                else {
+                    result = firstNum / secondNum;
+                }
+            }
+            case '^' -> result = Math.pow(firstNum, secondNum);
+            default -> {
+                System.out.print("Invalid operator!");
+                validOperation = false;
+            }
         }
 
-        if(Double.isInfinite(result)) {
-            resultMsg = "Cannot divide by zero!";
+        if(validOperation) {
+            System.out.printf("Your result: %.2f", result);
         }
-
-        System.out.printf("%s %.2f", resultMsg, result);
 
         scanner.close();
     }
