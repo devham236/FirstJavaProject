98abe09ed3f43f2e3fc0a7eff13e46743ed1e42d
Hamza Mirza <hamza665@hotmail.de>
2025-06-11 09:28:38 +0200

    clean up

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // switch statemtents
+        // enhanced switch statemtents (Java14 feature)
 
         Scanner scanner = new Scanner(System.in);
 
