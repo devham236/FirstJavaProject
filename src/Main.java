692d612d573612d242c453786d837ce28edda337
Hamza Mirza <hamza665@hotmail.de>
2025-06-11 10:36:15 +0200

    calculator program

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // enhanced switch statemtents (Java14 feature)
-
+        // calculator program
         Scanner scanner = new Scanner(System.in);
 
-        String day;
+        double firstNum;
+        double secondNum;
+        double result;
+        String operator;
+        String resultMsg = "Your result:";
 
-        System.out.print("What day is it today?: ");
-        day = scanner.nextLine();
+        System.out.print("Enter the first number: ");
+        firstNum = scanner.nextDouble();
 
-        switch(day) {
-            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> System.out.println("It is a weekday");
-            case "Saturday", "Sunday" -> System.out.println("It is the weekend");
-            default -> System.out.printf("%s is not a day", day);
+        System.out.print("Enter an operator: ");
+        operator = scanner.next();
+
+        System.out.print("Enter the second number: ");
+        secondNum = scanner.nextDouble();
+
+        switch(operator) {
+            case "+" -> result = firstNum + secondNum;
+            case "-" -> result = firstNum - secondNum;
+            case "*" -> result = firstNum * secondNum;
+            case "/" -> result = firstNum / secondNum;
+            default -> result = 0;
         }
+
+        if(Double.isInfinite(result)) {
+            resultMsg = "Cannot divide by zero!";
+        }
+
+        System.out.printf("%s %.2f", resultMsg, result);
+
+        scanner.close();
     }
 }
