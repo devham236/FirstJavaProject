3e6ce62f62aadeabc6a9b688b95ccd9d374e4058
Hamza Mirza <hamza665@hotmail.de>
2025-06-11 09:11:55 +0200

    switch statements

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // switch statemtents
 
+        String day = "Pizza";
+
+        switch(day) {
+            case "Monday" -> System.out.println("It is a weekday");
+            case "Tuesday" -> System.out.println("It is a weekday");
+            case "Wednesday" -> System.out.println("It is a weekday");
+            case "Thursday" -> System.out.println("It is a weekday");
+            case "Friday" -> System.out.println("It is a weekday");
+            case "Saturday" -> System.out.println("It is the weekend");
+            case "Sunday" -> System.out.println("It is the weekend");
+            default -> System.out.printf("%s is not a day", day);
+        }
     }
 }
