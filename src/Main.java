17cadec677e44dfcad3885868974369d451ff6a8
Hamza Mirza <hamza665@hotmail.de>
2025-06-11 09:27:18 +0200

    simplify switch statements

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,8 @@ public class Main {
         day = scanner.nextLine();
 
         switch(day) {
-            case "Monday" -> System.out.println("It is a weekday");
-            case "Tuesday" -> System.out.println("It is a weekday");
-            case "Wednesday" -> System.out.println("It is a weekday");
-            case "Thursday" -> System.out.println("It is a weekday");
-            case "Friday" -> System.out.println("It is a weekday");
-            case "Saturday" -> System.out.println("It is the weekend");
-            case "Sunday" -> System.out.println("It is the weekend");
+            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> System.out.println("It is a weekday");
+            case "Saturday", "Sunday" -> System.out.println("It is the weekend");
             default -> System.out.printf("%s is not a day", day);
         }
     }
