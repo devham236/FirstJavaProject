1292420e688981c3eafbaf26c1c99cc8587a9cd8
Hamza Mirza <hamza665@hotmail.de>
2025-06-19 10:01:25 +0200

    while loops

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // logical operators
+        // while loops
 
-        // && = AND
-        // || = OR
-        // ! = NOT
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        while (name.isEmpty()) {
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        System.out.printf("Hello %s", name);
+
+
+        scanner.close();
     }
 }
