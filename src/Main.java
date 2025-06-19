69d5aa43ddd0b1ec28ea2c2c7b03dab54ded335b
Hamza Mirza <hamza665@hotmail.de>
2025-06-19 10:08:29 +0200

    while loops extended

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,16 @@ public class Main {
         // while loops
 
         Scanner scanner = new Scanner(System.in);
-        String name = "";
+        String response = "";
 
-        while (name.isEmpty()) {
-            System.out.print("Enter your name: ");
-            name = scanner.nextLine();
+        while(!response.equals("Q")){
+            System.out.print("You are playing a game");
+            System.out.print("\nPress Q to quit: ");
+
+            response = scanner.nextLine().toUpperCase();
         }
 
-        System.out.printf("Hello %s", name);
-
+        System.out.println("You have quit the game.");
 
         scanner.close();
     }
