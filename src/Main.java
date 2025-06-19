b2f56bd08ea49a94310668965d52a50f21396a25
Hamza Mirza <hamza665@hotmail.de>
2025-06-19 11:40:07 +0200

    nested loops

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        // for loops
+        // nested loops
 
-        for(int i = 1; i <= 10; i++){
-            System.out.print(i);
+        for(int i = 1; i <= 3; i++){
+            for(int j = 1; j <= 9; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
         }
+
 
     }
 }
