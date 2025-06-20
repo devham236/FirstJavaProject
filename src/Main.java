be54b1c782012f93c907d8bf9088ea9a7531c0ee
Hamza Mirza <hamza665@hotmail.de>
2025-06-20 09:52:30 +0200

    methods

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        // methods
 
+        happyBirthday();
+    }
+
+    static void happyBirthday(){
+        System.out.println("Happy Birthday to you!");
+        System.out.println("Happy Birthday dear you!");
+        System.out.println("You are x years old!");
+        System.out.println("Happy Birthday to you!\n");
     }
 }
