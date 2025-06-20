fb8ba1750441ba83bb0b22e86f3153d819e53f9a
Hamza Mirza <hamza665@hotmail.de>
2025-06-20 09:00:50 +0200

    clean up

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
-        // nested loops
-
-        for(int i = 1; i <= 3; i++){
-            for(int j = 1; j <= 9; j++) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
-
 
     }
 }
