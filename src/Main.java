fde1710cab687e9c01047d8c893bbe4be2b4b6dc
Hamza Mirza <hamza665@hotmail.de>
2025-06-20 10:47:34 +0200

    local and calss scope

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,25 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
+    // Java prefers variables with local scope, that is why 1 and 2 gets
+    // printed to the console.
+
+    // class scope
+    static int x = 3;
+
     public static void main(String[] args) {
-        // variable scope
+        // local scope
+        int x = 1;
+
+        System.out.println(x);
+
+        doSomething();
+    }
+
+    static void doSomething() {
+        // local scope
+        int x = 2;
+
+        System.out.println(x);
     }
 }
