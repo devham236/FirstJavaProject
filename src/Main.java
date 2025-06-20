b26c5af3c5ccb0c565ce96d0e39fe75ba97edce6
Hamza Mirza <hamza665@hotmail.de>
2025-06-20 10:40:37 +0200

    clean up

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,6 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        // overloaded methods = methods that share the same name
-        //                      but different parameters
-        //                      signature = name + parameters
-        // Java takes the method with the matching parameters when two methods
-        // have the same name.
-
-        String pizza = bakePizza("Flat bread", "mozzarella");
-        System.out.println(pizza);
-    }
-
-    static String bakePizza(String bread) {
-        return bread + " pizza";
-    }
-
-    static String bakePizza(String bread, String cheese) {
-        return cheese + " " + bread + " pizza";
+        // variable scope
     }
 }
