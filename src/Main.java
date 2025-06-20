46ac6b320938d659c70f993cd230e9525083e572
Hamza Mirza <hamza665@hotmail.de>
2025-06-20 10:39:23 +0200

    overloaded methods with some explanation

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,18 @@ public class Main {
         // overloaded methods = methods that share the same name
         //                      but different parameters
         //                      signature = name + parameters
+        // Java takes the method with the matching parameters when two methods
+        // have the same name.
 
-        System.out.println(add(1, 2, 3));
+        String pizza = bakePizza("Flat bread", "mozzarella");
+        System.out.println(pizza);
     }
 
-    static double add(double a, double b){
-        return  a + b;
+    static String bakePizza(String bread) {
+        return bread + " pizza";
     }
 
-    static double add(double a, double b, double c){
-        return  a + b + c;
+    static String bakePizza(String bread, String cheese) {
+        return cheese + " " + bread + " pizza";
     }
 }
