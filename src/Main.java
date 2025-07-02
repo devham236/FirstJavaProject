0ed5299913f03450986b40020dc6acd79fd5ac3c
Hamza Mirza <hamza665@hotmail.de>
2025-07-02 14:39:25 +0200

    arrays

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // arrays
+        String[] fruits = {"apple", "banana", "orange"};
 
-
-
-
+        System.out.println(fruits[0]);
     }
 }
