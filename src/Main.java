2d3b6117a650c39c546297d210ec3feb0657be45
Hamza Mirza <hamza665@hotmail.de>
2025-07-02 14:50:38 +0200

    fill arrays with certain value

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // arrays
         String[] fruits = { "banana", "apple", "orange"};
-        Arrays.sort(fruits);
+        Arrays.fill(fruits, "watermelon");
 
         for (String fruit : fruits) {
             System.out.println(fruit);
