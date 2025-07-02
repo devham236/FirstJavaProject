44483d96a0e3470c124bf44c47c252c3c1ab75ba
Hamza Mirza <hamza665@hotmail.de>
2025-07-02 14:49:18 +0200

    sorting arrays using Array class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         // arrays
-        String[] fruits = {"apple", "banana", "orange"};
+        String[] fruits = { "banana", "apple", "orange"};
+        Arrays.sort(fruits);
 
         for (String fruit : fruits) {
             System.out.println(fruit);
