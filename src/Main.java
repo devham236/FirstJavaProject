0d533f0061843299560599e2319e02916bcfb0e7
Hamza Mirza <hamza665@hotmail.de>
2025-07-02 14:47:44 +0200

    printing array elements using enhanced for loop

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,8 @@ public class Main {
         // arrays
         String[] fruits = {"apple", "banana", "orange"};
 
-        // to print out all elements
-        // use a for loop, printing the array alone like this:
-        // System.out.println(fruits);
-        // prints the memory address to the console
-        for (int i = 0; i < fruits.length; i++){
-            System.out.println(fruits[i]);
+        for (String fruit : fruits) {
+            System.out.println(fruit);
         }
 
     }
