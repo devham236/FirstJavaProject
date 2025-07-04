e2e2fbdbfa160fdc823c6f965826ca6363f28d20
Hamza Mirza <hamza665@hotmail.de>
2025-07-04 12:57:00 +0200

    clean up

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
-        // arrays
-        String[] fruits = { "banana", "apple", "orange"};
-        Arrays.fill(fruits, "watermelon");
-
-        for (String fruit : fruits) {
-            System.out.println(fruit);
-        }
 
     }
 }
