1cca7dd7c5dbbc39d8005cfc129c0675fb6f393f
Hamza Mirza <hamza665@hotmail.de>
2025-07-04 13:15:54 +0200

    empty array brackets

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 
 public class Main {
     public static void main(String[] args) {
+        // Empty arrays should not be initialized with empty brackets
+        // It doesnt mean you can add elements to it by selecting indeces
+        // and setting it to some value
+        // Empty brackets mean the arrays size is zero, which means you cannot assign
+        // spaces/indeces in the array to a value
 
+        String[] foods = {};
+
+        foods[0] = "pizza";
+
+        System.out.println(foods);
     }
 }
