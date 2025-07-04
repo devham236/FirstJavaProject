673c485f82d554b1895cbf0cd8946d54cfd79871
Hamza Mirza <hamza665@hotmail.de>
2025-07-04 13:30:40 +0200

    enter user input into array with dynamic size

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Enter user input into an array
-        String[] foods = new String[3];
         Scanner scanner = new Scanner(System.in);
+        String[] foods;
+        int quantity = 3;
+
+        System.out.print("Enter the # of food items: ");
+        quantity = scanner.nextInt();
+        scanner.nextLine();
+        foods = new String[quantity];
 
         for (int i = 0; i < foods.length; i++) {
             System.out.print("Enter the foods you want: ");
