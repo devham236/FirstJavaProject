899f48e2bb97254aee96fe957bfdf904ed6072e6
Hamza Mirza <hamza665@hotmail.de>
2025-07-14 15:16:49 +0200

    clean up

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Enter user input into an array
-        Scanner scanner = new Scanner(System.in);
-        String[] foods;
-        int quantity = 3;
 
-        System.out.print("Enter the # of food items: ");
-        quantity = scanner.nextInt();
-        scanner.nextLine();
-        foods = new String[quantity];
-
-        for (int i = 0; i < foods.length; i++) {
-            System.out.print("Enter the foods you want: ");
-            foods[i] = scanner.next();
-        }
-
-        for (String food : foods) {
-            System.out.println(food);
-        }
-
-        scanner.close();
     }
 }
