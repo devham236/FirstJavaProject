dc130041abd25ec80b7c3a37a329475fc4491995
Hamza Mirza <hamza665@hotmail.de>
2025-07-15 15:11:29 +0200

    simple linear array search

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int target = 4;
 
+        for (int i = 0; i < numbers.length; i++) {
+            if (target == numbers[i]) {
+                System.out.printf("Element found at index: %d", i);
+            }
+        }
     }
 }
