c6834eebc5644694da7a439690ca0d98c0e81485
Hamza Mirza <hamza665@hotmail.de>
2025-07-15 15:22:00 +0200

    searching through a string array

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
-        int target = 9;
+        String[] fruits = {"apple", "kiwi", "banana"};
+        String target = "pineapple";
         boolean isFound = false;
 
-        for (int i = 0; i < numbers.length; i++) {
-            if (target == numbers[i]) {
+        // String elements cant be compared using "target==fruits[i]" as this would
+        // compare the memory addresses of both strings
+        // to compare two string values you have to use the string method "equals()"
+        for (int i = 0; i < fruits.length; i++) {
+            if (fruits[i].equals(target)) {
                 System.out.printf("Element found at index: %d", i);
                 isFound = true;
                 break;
