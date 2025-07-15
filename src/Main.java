9de480b0ec32c95b135a574168edc0752801aa19
Hamza Mirza <hamza665@hotmail.de>
2025-07-15 14:17:01 +0200

    clean up

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // If the array is empty, "adding" values by selecting an index is not possible
-        // as the arrays length is 0, meaning there is no index you can select
 
-        String[] foods = {};
-
-        foods[0] = "Pizza";
-
-
-        System.out.println(foods.length);
     }
 }
