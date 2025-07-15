fd7f5719041490f482e01cfd20e90bf630ce2850
Hamza Mirza <hamza665@hotmail.de>
2025-07-15 15:16:20 +0200

    show message when target is not found in array search

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
-        int target = 3;
+        int target = 9;
+        boolean isFound = false;
 
         for (int i = 0; i < numbers.length; i++) {
             if (target == numbers[i]) {
                 System.out.printf("Element found at index: %d", i);
+                isFound = true;
                 break;
             }
+        }
+
+        if (!isFound){
+            System.out.println("Element not found in the array");
         }
     }
 }
