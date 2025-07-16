b7b316645c88ad13478b6bde901888d6a3aa934e
Hamza Mirza <hamza665@hotmail.de>
2025-07-16 09:29:13 +0200

    varargs intro

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // varargs = variable arguments
+        // allows method to accept varying numbers of arguments
+        // arguments get packed into an array
+        System.out.println(add(1, 2, 3, 4, 5));
+    }
 
+    static int add(int... numbers) {
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        return sum;
     }
 }
