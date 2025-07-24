49ad1b9a4e861bc4256e65aaaa6f4e5363b9b1bd
Hamza Mirza <hamza665@hotmail.de>
2025-07-24 13:10:03 +0200

    created 2d array of options

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,11 @@ public class Main {
                               "What was the first programming language?"};
 
         // Create a 2-dimensional array of options
+        String[][] options = {{"1. Storing files", "2. Encrypting data", "3. Directing internet traffic", "4. Managing passwords"},
+                             {"1. CPU", "2. Hard Drive", "3. RAM", "4. GPU"},
+                             {"1. 2000", "2. 2004", "3. 2006", "4. 2008"},
+                             {"1. Steve Jobs", "2. Bill Gates", "3. Alan Turing", "4. Charles Babbage"},
+                             {"1. Cobol", "2. C", "3. Fortran", "4. Assembly"}};
 
         // Declare all neccessary variables
 
