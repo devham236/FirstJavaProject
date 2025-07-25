bf524fb7bb40e48b93aabcfb210983f89a40a6fb
Hamza Mirza <hamza665@hotmail.de>
2025-07-25 12:41:40 +0200

    info about game

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Rock Paper Scissors
+
+        // rock > scissors
+        // scissors > paper
+        // paper > rock
     }
 }
