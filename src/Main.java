d63405912d4dfd3748ea219c22617db75f740fef
Hamza Mirza <hamza665@hotmail.de>
2025-07-25 12:47:10 +0200

    flow of the program

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Rock Paper Scissors
+        // Rock Paper Scissors Game
 
         // rock > scissors
         // scissors > paper
         // paper > rock
+
+        // Player enters move (rock, paper, scissors)
+        // Computer shows his choice
+
+        // When player wins, show winning message
+        // When player loses, show losing message
+
+        // Ask if player wants to play again
+
     }
 }
