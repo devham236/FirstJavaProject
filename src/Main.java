284024613a5564d91a96cf2f96dc38d496b034b3
Hamza Mirza <hamza665@hotmail.de>
2025-07-25 12:38:49 +0200

    clean up

--- src/Main.java
+++ src/Main.java
@@ -2,57 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Quiz Game
 
-        // Create an array of questions
-        String[] questions = {"What is the main function of a router?",
-                              "Which part of the computer is considered the brain?",
-                              "What year was facebook launched?", "" +
-                              "Who is known as the father of computers?",
-                              "What was the first programming language?"};
-
-        // Create a 2-dimensional array of options
-        String[][] options = {{"1. Storing files", "2. Encrypting data", "3. Directing internet traffic", "4. Managing passwords"},
-                             {"1. CPU", "2. Hard Drive", "3. RAM", "4. GPU"},
-                             {"1. 2000", "2. 2004", "3. 2006", "4. 2008"},
-                             {"1. Steve Jobs", "2. Bill Gates", "3. Alan Turing", "4. Charles Babbage"},
-                             {"1. Cobol", "2. C", "3. Fortran", "4. Assembly"}};
-
-        // Declare all necessary variables
-        int[] answers = {3, 1, 2, 4, 3};
-        int score = 0;
-        int guess;
-        Scanner scanner = new Scanner(System.in);
-
-        // Print a welcome message
-        System.out.println("******************************");
-        System.out.println("Welcome to the Java Quiz Game!");
-        System.out.println("******************************");
-
-        // Loop over arrays and display contents
-        for (int i = 0; i < questions.length; i++){
-            System.out.println(questions[i]);
-            // display options
-            for (String option : options[i]){
-                System.out.println(option);
-            }
-            // get a guess from the user
-            System.out.print("Enter your guess: ");
-            guess = scanner.nextInt();
-
-            // check the guess
-            if(guess == answers[i]){
-                System.out.println("Correct!");
-                score++;
-            }
-            else {
-                System.out.println("Wrong!");
-            }
-        }
-
-        // Display final score
-        System.out.printf("Your final score is: %d/%d", score, questions.length);
-
-        scanner.close();
     }
 }
