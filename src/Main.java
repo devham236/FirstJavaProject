1cb4850d498273cd8c2acdd057b3f0d9a1c26ef4
Hamza Mirza <hamza665@hotmail.de>
2025-08-01 14:10:51 +0200

    declared variable

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // Rock Paper Scissors Game
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] choices = {"rock", "paper", "scissors"};
+        String playerChoice;
+        String computerChoice;
+        String playAgain = "yes";
 
         scanner.close();
     }
