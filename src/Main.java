05911b810920c060bb2b5ada32f05c3c7d4fac12
Hamza Mirza <hamza665@hotmail.de>
2025-08-01 14:35:38 +0200

    move logic to do while loop and added end of game message

--- src/Main.java
+++ src/Main.java
@@ -12,30 +12,38 @@ public class Main {
         String computerChoice;
         String playAgain = "yes";
 
-        System.out.print("Enter your move (rock, paper, scissors): ");
-        playerChoice = scanner.nextLine().toLowerCase();
+        do {
+            System.out.print("\nEnter your move (rock, paper, scissors): ");
+            playerChoice = scanner.nextLine().toLowerCase();
 
-        if (!playerChoice.equals("rock") &&
-                !playerChoice.equals("paper") &&
-                !playerChoice.equals("scissors")){
-            System.out.print("Invalid choice");
-        }
+            if (!playerChoice.equals("rock") &&
+                    !playerChoice.equals("paper") &&
+                    !playerChoice.equals("scissors")){
+                System.out.print("Invalid choice");
+                continue;
+            }
 
-        computerChoice = choices[random.nextInt(3)];
-        System.out.println("Computer choice: " + computerChoice);
+            computerChoice = choices[random.nextInt(3)];
+            System.out.println("Computer choice: " + computerChoice);
 
-        if (playerChoice.equals(computerChoice)){
-            System.out.println("It's a tie!");
-        } else if (playerChoice.equals("rock") && computerChoice.equals("scissors")) {
-            System.out.println("You win!");
-        } else if (playerChoice.equals("paper") && computerChoice.equals("rock")) {
-            System.out.println("You win!");
-        } else if (playerChoice.equals("scissors") && computerChoice.equals("paper")) {
-            System.out.println("You win!");
-        }
-        else {
-            System.out.print("You lose!");
-        }
+            if (playerChoice.equals(computerChoice)){
+                System.out.println("It's a tie!");
+            } else if (playerChoice.equals("rock") && computerChoice.equals("scissors")) {
+                System.out.println("You win!");
+            } else if (playerChoice.equals("paper") && computerChoice.equals("rock")) {
+                System.out.println("You win!");
+            } else if (playerChoice.equals("scissors") && computerChoice.equals("paper")) {
+                System.out.println("You win!");
+            }
+            else {
+                System.out.println("You lose!");
+            }
+
+            System.out.print("Play again? (yes/no): ");
+            playAgain = scanner.nextLine().toLowerCase();
+        } while (playAgain.equals("yes"));
+
+        System.out.print("Thanks for playing!");
 
         scanner.close();
     }
